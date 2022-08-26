1b95abc11cb029b4cac97eda5afc37f9c70a57c1
Casper Posche <cbp@posche.dk>
2022-08-26 12:39:44 +0200

    enviromental read

--- src/main/java/DBConnector.java
+++ src/main/java/DBConnector.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-    private static String URL = "jdbc:mysql://164.92.175.138:3306/startcode_test?serverTimezone=Europe/Copenhagen";
-    private static String USER = "dev";
-    private static String PW = "3r!DE32*/fDe";
+    private static String URL = "jdbc:mysql://localhost:3306/startcode_test?serverTimezone=Europe/Copenhagen";
+    private static String USER = System.getenv("USER");
+    private static String PW = System.getenv("PW");
 
     private static Connection singleton;
 
